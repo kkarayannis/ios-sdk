fe18cf5c357464cb6a260f0159f9e11dddf8e601
Konstantinos Karagiannis <konstantinosk@spotify.com>
2019-03-15 12:02:02 +0100

    Added a record button to demonstrate how the app remote behaves when it is interrupted by AVAudioSession

--- DemoProjects/NowPlayingView/NowPlayingView/ViewController.swift
+++ DemoProjects/NowPlayingView/NowPlayingView/ViewController.swift
@@ -1,11 +1,13 @@
 import UIKit
 import StoreKit
+import AVFoundation
 
 class ViewController: UIViewController,
                       SPTAppRemotePlayerStateDelegate,
                       SPTAppRemoteUserAPIDelegate,
                       SpeedPickerViewControllerDelegate,
-                      SKStoreProductViewControllerDelegate {
+                      SKStoreProductViewControllerDelegate,
+AVAudioRecorderDelegate {
 
     fileprivate let playURI = "spotify:album:5uMfshtC2Jwqui0NUyUYIL"
     fileprivate let trackIdentifier = "spotify:track:32ftxJzxMPgUFCM6Km9WTS"
@@ -39,6 +41,8 @@ class ViewController: UIViewController,
         skipForward15Button.setImage(skipForward15Button.imageView?.image?.withRenderingMode(.alwaysTemplate), for: .normal)
         skipBackward15Button.isHidden = true
         skipForward15Button.isHidden = true
+
+        setupRecording()
     }
 
     // MARK: - View
@@ -505,4 +509,91 @@ class ViewController: UIViewController,
         })
         viewController.dismiss(animated: true, completion: nil)
     }
+
+    // MARK: - Recording Test
+
+    @IBOutlet var recordButton: UIButton!
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+
+    func loadRecordingUI() {
+        recordButton.isHidden = false
+    }
+
+    func setupRecording() {
+        recordingSession = AVAudioSession.sharedInstance()
+
+        do {
+            if #available(iOS 10.0, *) {
+                try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryRecord, mode: AVAudioSessionModeDefault)
+            } else {
+                // Fallback on earlier versions
+            }
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        self.loadRecordingUI()
+                    } else {
+                        // failed to record!
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
+    }
+
+    @IBAction func didTapRecord(_ sender: Any) {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+
+            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
+    }
+
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+
 }
